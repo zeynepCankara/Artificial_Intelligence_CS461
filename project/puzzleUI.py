3ab2d0abf193a8f5cf0f3457c872f90ee9d73c09
ege-sahin <sahin.ege98@gmail.com>
2021-02-26 21:04:37 +0300

    UI result of parsePuzzle

---
+++ project/puzzleUI.py
@@ -0,0 +1,53 @@
+from tkinter import * 
+from parsePuzzle import parsePuzzle
+
+puzzleInformation = parsePuzzle()
+
+# creating main tkinter window/toplevel 
+master = Tk() 
+
+# Creating Title of Across Clues
+l0 = Label(master, text = "Across clues:")
+l0.grid(row = 0, column = 5, rowspan = 1)
+
+# Creating Across Clues
+i = 0
+acrossClues = []
+for clueNumber, clue in puzzleInformation['acrossClues'].items():
+    acrossClues.append(Label(master, text = (clueNumber, clue)))
+    acrossClues[i].grid(row = i + 1, column = 5, sticky = W, rowspan = 1)
+    i += 1
+
+# Creating Title of Down Clues
+i += 1
+l1 = Label(master, text = "Down clues:")
+l1.grid(row = i, column = 5, rowspan = 1)
+
+# Creating Down Clues
+downClues = []
+j = 0
+for clueNumber, clue in puzzleInformation['downClues'].items():
+    downClues.append(Label(master, text = (clueNumber, clue)))
+    downClues[j].grid(row = i + 1, column = 5, sticky = W, rowspan = 1)
+    i += 1
+    j += 1
+
+# Creating puzzle Information
+i = 0
+boxList = []
+for cell in puzzleInformation['cells']:
+    if(cell["cellNumber"] == -1):
+        boxList.append(Label(master, bg="black", borderwidth=2, relief="ridge", height=5, width=10))
+    else:
+        if(cell["cellNumber"] != 0):
+            boxList.append(Label(master, bg="white", borderwidth=2, relief="solid", height=5, width=10, text = cell['cellNumber'], anchor = "nw"))
+        else:
+            boxList.append(Label(master, bg="white", borderwidth=2, relief="solid", height=5, width=10))
+    boxList[i].grid(row = (int(i / 5) * 3), column = (i % 5), rowspan = 3)
+    print(int(i / 5) * 4)
+    i += 1    
+  
+mainloop() 
+
+
+#If you want a border, the option is borderwidth. You can also choose the relief of the border: "flat", "raised", "sunken", "ridge", "solid", and "groove".
